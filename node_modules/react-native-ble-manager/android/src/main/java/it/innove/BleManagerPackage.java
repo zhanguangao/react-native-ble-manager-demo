6a9705f0b7420c922ea22f6e847800243660e53b
幻影 <190525238@qq.com>
2017-05-15 15:20:13 +0800

    react-native-ble-manager BLE蓝牙通信demo

---
+++ node_modules/react-native-ble-manager/android/src/main/java/it/innove/BleManagerPackage.java
@@ -0,0 +1,35 @@
+package it.innove;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class BleManagerPackage implements ReactPackage {
+
+	public BleManagerPackage() {}
+
+	@Override
+	public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
+		List<NativeModule> modules = new ArrayList<>();
+
+		modules.add(new BleManager(reactApplicationContext));
+		return  modules;
+	}
+
+	@Override
+	public List<Class<? extends JavaScriptModule>> createJSModules() {
+		return new ArrayList<>();
+	}
+
+	@Override
+	public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
+		return Collections.emptyList();
+	}
+}
---
+++ node_modules/react-native-ble-manager/ios/BleManager.h
@@ -0,0 +1,23 @@
+#import "React/RCTBridgeModule.h"
+#import "React/RCTEventEmitter.h"
+#import <CoreBluetooth/CoreBluetooth.h>
+
+
+@interface BleManager : NSObject <RCTBridgeModule, CBCentralManagerDelegate, CBPeripheralDelegate>{
+    NSString* discoverPeripherialCallbackId;
+    NSMutableDictionary* connectCallbacks;
+    NSMutableDictionary *readCallbacks;
+    NSMutableDictionary *writeCallbacks;
+    NSMutableDictionary *readRSSICallbacks;
+    NSMutableArray *writeQueue;
+    NSMutableDictionary *notificationCallbacks;
+    NSMutableDictionary *stopNotificationCallbacks;
+    NSMutableDictionary *connectCallbackLatches;
+}
+
+@property (strong, nonatomic) NSMutableSet *peripherals;
+@property (strong, nonatomic) CBCentralManager *manager;
+@property (weak, nonatomic) NSTimer *scanTimer;
+
+
+@end
